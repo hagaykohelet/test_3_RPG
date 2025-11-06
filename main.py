cc740479198ef23aaa753daf6325a1d4b3dd8392
hagay kohelet <hagay2506@gmail.com>
2025-11-06 12:50:04 +0200

    RPG test

--- main.py
+++ main.py
@@ -3,5 +3,8 @@ from core.goblin import Goblin
 from core.player import General
 from core.player import Player
 from game import Game
-
+player = Player("hagay")
 game = Game()
+monster = game.choose_random_monster()
+game.show_menu()
+game.battle(player,monster,game.show_menu(),game.roll_dice(6),game.roll_dice(20))
